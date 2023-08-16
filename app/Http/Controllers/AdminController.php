ba0fcf44452336093063d80fa90983264f7d3cef
JayAlwaysCodes <johnsonjardocs@gmail.com>
2023-08-16 06:31:53 +0100

    finished the admin panel basic set up

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function AdminDashboard(){
-        return view('admin.admin_dashboard');
+        return view('admin.index');
     }//End Method
 }
