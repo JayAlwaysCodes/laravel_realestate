88c0fb365042f26c4a8c204b96d78da8304fa49b
JayAlwaysCodes <johnsonjardocs@gmail.com>
2023-08-16 07:21:38 +0100

    worked on the logout logic of the admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,8 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth','role:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 }); //End group admin middleware
 
 Route::middleware(['auth','role:agent'])->group(function(){
