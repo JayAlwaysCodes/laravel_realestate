9c4ae911e55978d47fd05f8b4b5959cc0b79886a
JayAlwaysCodes <johnsonjardocs@gmail.com>
2023-08-16 14:00:23 +0100

    started working on the admin profile

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 
 class AdminController extends Controller
@@ -26,5 +27,11 @@ class AdminController extends Controller
 
     public function AdminLogin(){
         return view('admin.admin_login');
-    }
+    }//End method
+
+    public function AdminProfile(){
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view', compact('profileData'));
+    }//end method
 }
